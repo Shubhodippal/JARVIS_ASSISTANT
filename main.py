de2a424089991d845700c8cfd9b57c48a7bbb6d0
Shubhodip Pal <shubhodippal01@gmail.com>
2024-01-08 21:37:59 +0530

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import api
 import speech_recognition as sr
 import pyttsx3
 import datetime
@@ -7,7 +8,6 @@ import os
 import random
 import smtplib
 import openai
-import api
 import locationtest
 import data
 import Weathertest
